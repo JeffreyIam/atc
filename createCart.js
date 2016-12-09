5abe6e491d64e2246f9ed6ef677db7d5d3ea9fff
Jeff Lam <jefflam1200@gmail.com>
2016-12-09 01:47:10 -0800

    Added stock qty and size fetching

--- createCart.js
+++ createCart.js
@@ -1,8 +1,30 @@
 const link = window.location.pathname.split("/")
 const sitekey = link[link.length-1]
 
-function changeView(sitekey) {
-  $('body').html('<head><script src="https://www.google.com/recaptcha/api.js"></script></head><div class="jumbotron vertical-center" style="min-height: 100%; min-height: 100vh; display: flex; align-items: center"><div class="container" style="text-align: center; border: 2px solid black; width: 500px"><h3>Add Item To Cart:</h3><form action="http://example.net" method="get"><input type="text" name="style" class="form-control" style="width: 100px; border-radius:75px; text-align: center; margin: auto; margin-bottom: 5px" placeholder="SKU"><input placeholder="Quantity" class="form-control" style="width: 100px; margin: auto; border-radius:75px; margin-bottom: 5px; text-align: center" name="quantity"/><input type="text" name="capdupfieldname" class="form-control" style="width: 100px; border-radius: 75px; text-align: center; margin: auto; margin-bottom: 5px" placeholder="Cap Dup"/><select class="form-control" style="width:100px; border-radius:75px; margin: auto; margin-bottom: 5px" name="size"><option value="">Select Size</option><option value="_240">5k</option><option value="_260">6k</option><option value="_310">8k</option><option value="_330">9k</option><option value="_350">10k</option><option value="_530">4</option> <option value="_540">4.5</option> <option value="_550">5</option> <option value="_560">5.5</option> <option value="_570">6</option> <option value="_580">6.5</option> <option value="_590">7</option> <option value="_600">7.5</option> <option value="_610">8</option> <option value="_620">8.5</option> <option value="_630">9</option> <option value="_640">9.5</option> <option value="_650">10</option> <option value="_660">10.5</option> <option value="_670">11</option> <option value="_680">11.5</option> <option value="_690">12</option> <option value="_700">12.5</option> <option value="_710">13</option> <option value="_720">13.5</option> <option value="_730">14</option> <option value="_750">15</option></select><input class="btn btn-primary" style="margin-bottom: 5px" type="submit" name="submit_captcha" value="Add To Cart"/><br><div class="g-recaptcha" style="margin: auto; display: inline-block; margin-bottom: 5px" async data-sitekey="'+ sitekey +'"></div></form></div></div>')
+function changeView(sitekey, fun) {
+
+  $('body').html('<head><script src="https://www.google.com/recaptcha/api.js"></script></head><div class="jumbotron vertical-center" style="min-height: 100%; min-height: 100vh; display: flex; align-items: center"><div class="container" style="text-align: center; border: 2px solid black; width: 500px"><h3>Add Item To Cart:</h3><form action="http://example.net" method="get"><input id="sku" type="text" name="style" class="form-control" style="width: 100px; border-radius:75px; text-align: center; margin: auto; margin-bottom: 5px" placeholder="SKU"><input placeholder="Quantity" class="form-control" style="width: 100px; margin: auto; border-radius:75px; margin-bottom: 5px; text-align: center" name="quantity"/><input type="text" name="capdupfieldname" class="form-control" style="width: 100px; border-radius: 75px; text-align: center; margin: auto; margin-bottom: 5px" placeholder="Cap Dup"/><select class="form-control" style="width:100px; border-radius:75px; margin: auto; margin-bottom: 5px" name="size"><option value="">Select Size</option><option value="_240">5k</option><option value="_260">6k</option><option value="_310">8k</option><option value="_330">9k</option><option value="_350">10k</option><option value="_530">4</option> <option value="_540">4.5</option> <option value="_550">5</option> <option value="_560">5.5</option> <option value="_570">6</option> <option value="_580">6.5</option> <option value="_590">7</option> <option value="_600">7.5</option> <option value="_610">8</option> <option value="_620">8.5</option> <option value="_630">9</option> <option value="_640">9.5</option> <option value="_650">10</option> <option value="_660">10.5</option> <option value="_670">11</option> <option value="_680">11.5</option> <option value="_690">12</option> <option value="_700">12.5</option> <option value="_710">13</option> <option value="_720">13.5</option> <option value="_730">14</option> <option value="_750">15</option></select><input class="btn btn-primary" style="margin-bottom: 5px" type="submit" name="submit_captcha" value="Add To Cart"/><br><div class="g-recaptcha" style="margin: auto; display: inline-block; margin-bottom: 5px" async data-sitekey="'+ sitekey +'"></div></form></div><div class="container" style="text-align: center; border: 2px solid black; width: 500px"><h3>Check Stock Inventory:</h3><button id="createTable" class="btn btn-primary" style="margin-bottom: 5px">Check Inv</button><div style="width: 350px; height: 600px; overflow: auto"><table id="inventory" class="table table-striped table-bordered table-condensed table-hover" style="align: center"></table/></div></div></div>')
+
+  document.querySelector('#createTable').addEventListener('click', () => {
+    const sku = document.querySelector('#sku').value.toUpperCase()
+    const sizeAndStock = document.querySelector('#inventory')
+
+    $.ajax({
+      url: "http://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/Product-GetVariants?pid=" + sku,
+      type: 'GET',
+      success: function(response) {
+        const productDetails = response.variants !== null ? response.variations.variants : []
+        console.log(productDetails)
+        const rowsAndCols = productDetails.length > 0 ? productDetails.reduce(function(total, current) {
+          const stockQty = current.ATS
+          const size = current.attributes.size
+          total += '<tr><td>'+ size + '</td>' + '<td>'+ stockQty + '</td></tr>'
+          return total
+        },'') : (function() {alert('SKU incorrect/Product cannot be found at the moment')})()
+        sizeAndStock.innerHTML = '<tbody><tr><th>Size(US)</th><th>Stock</th></tr>' + rowsAndCols + '</tbody>'
+      }
+    })
+  })
 }
 
 changeView(sitekey)
