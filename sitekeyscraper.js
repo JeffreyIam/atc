fae3a1074e2bb0a3361a22d2f94c25428de64f38
Jeff Lam <jefflam1200@gmail.com>
2017-01-08 03:31:07 -0800

    add sitescraper for sitekey

---
+++ sitekeyscraper.js
@@ -0,0 +1,42 @@
+const request = require('request')
+const zlib = require('zlib')
+const parseString = require('xml2js').parseString
+
+const grabLink = (url) => {
+  let options = {
+    method: 'GET',
+    uri: url,
+    gzip: true
+  }
+  request(options, (err, res, body) => {
+    parseString(body, (err, result) => {
+      if (err) {
+        console.log(err)
+        console.log('Link Error - Please check your url: ' + url + ' / or you got IP banned')
+        return
+      }
+      let items = result.urlset['url']
+      items.forEach((value) => {
+        let pageUrl = value.loc[0]
+        if(pageUrl.indexOf('shoes') > -1) {
+          request(pageUrl, (error, res, body) => {
+            console.log(res)
+            if(body !== undefined && body.indexOf('data-sitekey') > -1) {
+              console.log('Sitekey found on: ' + pageUrl)
+              let sitekey = body.split('data-sitekey="')[1].split('"><')[0]
+              console.log(sitekey)
+            } else if(body === undefined){
+              console.log(pageUrl + ' body is undefined')
+            }
+          })
+        }
+      })
+    })
+  })
+}
+
+const sitemapList = ['']
+
+sitemapList.forEach((url) => {
+  grabLink(url)
+})
